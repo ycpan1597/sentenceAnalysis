741e7a18c3e23041bb71b6e4807c58fa7a092048
ycpan1597 <ycpan@uw.edu>
2018-12-18 17:25:33 +0800

    made changes to the script

--- sentenceProcessing.py
+++ sentenceProcessing.py
@@ -7,46 +7,58 @@ Created on Sun Dec  9 09:37:02 2018
 """
 import matplotlib.pyplot as plt
 import numpy as np
-
+import os
+import re
 
 class wordAnalysis:
-    def __init__(self, fileName):
-        self.content = self.readFile(fileName)
-        self.wordsInSent, self.sentences, self.avg, self.std = self.findWordsInSent(self.content)
+    def __init__(self, fileName, location = 'sample files/'):
+        self.fileName = fileName
+        self.content = self.readFile(fileName, location)
+        self.wordsInSent, self.longSentences, self.avg, self.std = self.findWordsInSent(self.content)
         
         plt.figure()
         plt.hist(self.wordsInSent, bins = np.arange(0, 35, 1))
-        plt.title('Words in each sentence, (total = ' + str(len(self.sentences)) + ' sentences)')
+        plt.title(fileName + ' (total = ' + str(len(self.longSentences)) + ' sentences)')
         plt.axvline(x = self.avg, label = 'Avg = ' + str(self.avg) + ', Std = ' + str(self.std), color = 'k')
         plt.axvline(x = self.avg + self.std, color = 'k', ls = '--')
         plt.axvline(x = self.avg - self.std, color = 'k', ls = '--')
-        plt.axvline(x = 20, label = 'too many words!', color = 'r', ls = '--')
-        plt.axvline(x = 4, label = 'too few words!', color = 'r', ls = '--')
+#        plt.axvline(x = 20, label = 'too many words!', color = 'r', ls = '--')
+#        plt.axvline(x = 4, label = 'too few words!', color = 'r', ls = '--')
         plt.legend()
         
-    def readFile(self, fileName):
-        with open(fileName, 'r') as f:
+    def readFile(self, fileName, location):
+        with open(location + fileName, 'r') as f:
             x = f.readlines()
         return x
     
     def findWordsInSent(self, content, threshLow = 3):
         wordsInSent = [] #number of words in each sentence
-        sentences = []
+        longSentences = []
         for oneP in content:
-            allS = oneP.split('. ')
+            allS = re.split(r'\n', oneP)
+#            allS = oneP.split('. ')
             for oneS in allS:
                 curLength = len(oneS.split(' '))
                 wordsInSent.append(curLength)
-                sentences.append(oneS)
+                longSentences.append(oneS)
         for index, item in enumerate(wordsInSent):
             if item <= threshLow:
                 wordsInSent.remove(item)
-                del sentences[index]
-        return wordsInSent, sentences, round(sum(wordsInSent)/len(sentences), 2), round(np.std(wordsInSent), 2)
+                del longSentences[index]
+        return wordsInSent, longSentences, round(sum(wordsInSent)/len(longSentences), 2), round(np.std(wordsInSent), 2)
 
 plt.close('all')
-writing1 = wordAnalysis('Personal Statement - BME.txt')
-writing2 = wordAnalysis('Personal Statement - BME(2).txt')
+#reads all files in a directory and returns a dictionary of wordAnalysis objects
+def readAll(location = 'sample files'):
+    fileDic = {}
+    for oneFileName in os.listdir(location):
+        fileDic[oneFileName] = wordAnalysis(oneFileName)
+    return fileDic
+
+dic = readAll()
+        
+    
+
 
 
 
